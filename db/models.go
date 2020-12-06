bbe18cbf0e47b5b0be91160a427e0eae65d9e371
Dalton Scharff <dalton.scharff@gmail.com>
2020-12-06 14:59:45 -0600

    adds structs for each table

--- db/models.go
+++ db/models.go
@@ -1,1 +1,32 @@
 package db
+
+type Puzzle struct {
+	ID      int
+	Date    string
+	Letters []byte
+	Center  byte
+}
+
+type Find struct {
+	ID         int
+	WordID     int
+	RoomID     int
+	PlayerName string
+	FoundAt    string
+}
+
+type Room struct {
+	ID   int
+	Code string
+}
+
+type Word struct {
+	ID          int
+	Word        string
+	PuzzleID    int
+	Points      int
+	Definitions []struct {
+		Definition   string
+		PartOfSpeech string
+	}
+}
