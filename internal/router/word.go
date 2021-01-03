ae0ca2bf399d2ac9d5d7f87ef4378fef28e287e2
Dalton Scharff <dalton.scharff@gmail.com>
2021-01-03 11:59:46 -0600

    adds duplicated code to one 'queryWords' func

--- internal/router/word.go
+++ internal/router/word.go
@@ -19,24 +19,34 @@ type Word struct {
 	Synonym      string `json:"synonym"`
 }
 
-func viewAllWords(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+func queryWords(query string, args ...interface{}) ([]Word, error) {
 	db, err := db.Connect()
 	if err != nil {
-		fmt.Fprintf(w, "Error: could not connect to db")
+		return nil, err
 	}
 	defer db.Close()
 
-	rows, err := db.Query("SELECT id, word, point_value, definition, part_of_speech, synonym, puzzle_id FROM words;")
+	rows, err := db.Query(query, args...)
 	if err != nil {
-		http.Error(w, "Could not execute query", http.StatusInternalServerError)
-		return
+		return nil, err
 	}
 
-	var response []Word
+	response := []Word{}
 	for rows.Next() {
 		word := Word{}
-		rows.Scan(&word.ID, &word.Word, &word.PointValue, &word.Definition, &word.PartOfSpeech, &word.Synonym, &word.PuzzleID)
+		rows.Scan(&word.ID, &word.Word, &word.PuzzleID, &word.PointValue, &word.Definition, &word.PartOfSpeech, &word.Synonym)
 		response = append(response, word)
+	}
+
+	return response, nil
+}
+
+func viewAllWords(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	response, err := queryWords("SELECT * FROM words;")
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
@@ -44,23 +54,11 @@ func viewAllWords(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 }
 
 func viewWord(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	db, err := db.Connect()
+	response, err := queryWords("SELECT * FROM words WHERE id = $1;", p.ByName("id"))
 	if err != nil {
-		fmt.Fprintf(w, "Error: could not connect to db")
-	}
-	defer db.Close()
-
-	rows, err := db.Query("SELECT id, word, point_value, definition, part_of_speech, synonym, puzzle_id FROM words WHERE id = $1;", p.ByName("id"))
-	if err != nil {
-		http.Error(w, "Could not execute query", http.StatusInternalServerError)
+		fmt.Println(err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
-	}
-
-	var response []Word
-	for rows.Next() {
-		word := Word{}
-		rows.Scan(&word.ID, &word.Word, &word.PointValue, &word.Definition, &word.PartOfSpeech, &word.Synonym, &word.PuzzleID)
-		response = append(response, word)
 	}
 
 	w.Header().Set("Content-Type", "application/json")
@@ -78,104 +76,86 @@ func createWord(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	var word Word
 	json.NewDecoder(r.Body).Decode(&word)
 
-	db, err := db.Connect()
-	if err != nil {
-		fmt.Fprintf(w, "Error: could not connect to db")
-	}
-	defer db.Close()
-
-	rows, err := db.Query("INSERT INTO words (word, point_value, definition, part_of_speech, synonym, puzzle_id) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;", word.Word, word.PointValue, word.Definition, word.PartOfSpeech, word.Synonym, word.PuzzleID)
+	response, err := queryWords("INSERT INTO words (word, point_value, definition, part_of_speech, synonym, puzzle_id) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;", word.Word, word.PointValue, word.Definition, word.PartOfSpeech, word.Synonym, word.PuzzleID)
 	if err != nil {
 		fmt.Println(err)
-		http.Error(w, "Could not execute query", http.StatusBadRequest)
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	var response []Word
-	for rows.Next() {
-		word := Word{}
-		rows.Scan(&word.ID, &word.Word, &word.PuzzleID, &word.PointValue, &word.Definition, &word.PartOfSpeech, &word.Synonym)
-		response = append(response, word)
-	}
-
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(response[0])
+
+	switch len(response) {
+	case 0:
+		json.NewEncoder(w).Encode(struct{}{})
+		break
+	default:
+		json.NewEncoder(w).Encode(response[0])
+	}
 }
 
 func updateWord(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	var word Word
 	json.NewDecoder(r.Body).Decode(&word)
 
-	db, err := db.Connect()
-	if err != nil {
-		fmt.Fprintf(w, "Error: could not connect to db")
-	}
-	defer db.Close()
-
-	rows, err := db.Query("UPDATE words SET word = $1, point_value = $2, definition = $3, part_of_speech = $4, synonym = $5, puzzle_id = $6 WHERE id = $7 RETURNING *;", word.Word, word.PointValue, word.Definition, word.PartOfSpeech, word.Synonym, word.PuzzleID, p.ByName("id"))
+	response, err := queryWords("UPDATE words SET word = $1, point_value = $2, definition = $3, part_of_speech = $4, synonym = $5, puzzle_id = $6 WHERE id = $7 RETURNING *;", word.Word, word.PointValue, word.Definition, word.PartOfSpeech, word.Synonym, word.PuzzleID, p.ByName("id"))
 	if err != nil {
 		fmt.Println(err)
-		http.Error(w, "Could not execute query", http.StatusBadRequest)
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	var response []Word
-	for rows.Next() {
-		word := Word{}
-		rows.Scan(&word.ID, &word.Word, &word.PuzzleID, &word.PointValue, &word.Definition, &word.PartOfSpeech, &word.Synonym)
-		response = append(response, word)
-	}
-
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(response[0])
+
+	switch len(response) {
+	case 0:
+		json.NewEncoder(w).Encode(struct{}{})
+		break
+	default:
+		json.NewEncoder(w).Encode(response[0])
+	}
 }
 
 func deleteAllWords(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	db, err := db.Connect()
-	if err != nil {
-		fmt.Fprintf(w, "Error: could not connect to db")
-	}
-	defer db.Close()
+	var word Word
+	json.NewDecoder(r.Body).Decode(&word)
 
-	rows, err := db.Query("DELETE FROM words RETURNING *;")
+	response, err := queryWords("DELETE FROM words RETURNING *;")
 	if err != nil {
 		fmt.Println(err)
-		http.Error(w, "Could not execute query", http.StatusBadRequest)
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	var response []Word
-	for rows.Next() {
-		word := Word{}
-		rows.Scan(&word.ID, &word.Word, &word.PuzzleID, &word.PointValue, &word.Definition, &word.PartOfSpeech, &word.Synonym)
-		response = append(response, word)
-	}
-
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(response)
+
+	switch len(response) {
+	case 0:
+		json.NewEncoder(w).Encode(struct{}{})
+		break
+	default:
+		json.NewEncoder(w).Encode(response[0])
+	}
 }
 
 func deleteWord(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	db, err := db.Connect()
-	if err != nil {
-		fmt.Fprintf(w, "Error: could not connect to db")
-	}
-	defer db.Close()
+	var word Word
+	json.NewDecoder(r.Body).Decode(&word)
 
-	rows, err := db.Query("DELETE FROM words WHERE id = $1 RETURNING *;", p.ByName("id"))
+	response, err := queryWords("DELETE FROM words WHERE id = $1 RETURNING *;", p.ByName("id"))
 	if err != nil {
 		fmt.Println(err)
-		http.Error(w, "Could not execute query", http.StatusBadRequest)
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	var response []Word
-	for rows.Next() {
-		word := Word{}
-		rows.Scan(&word.ID, &word.Word, &word.PuzzleID, &word.PointValue, &word.Definition, &word.PartOfSpeech, &word.Synonym)
-		response = append(response, word)
-	}
-
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(response[0])
+
+	switch len(response) {
+	case 0:
+		json.NewEncoder(w).Encode(struct{}{})
+		break
+	default:
+		json.NewEncoder(w).Encode(response[0])
+	}
 }
