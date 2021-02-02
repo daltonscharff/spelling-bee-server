299d17ca855ecadd814ccf26647d4c6fea80fa73
Dalton Scharff <dalton.scharff@gmail.com>
2021-02-01 22:10:21 -0600

    Renames words file to wordsTable and handler folder to handlers

--- internal/router/router.go
+++ internal/router/router.go
@@ -3,19 +3,19 @@ package router
 import (
 	"github.com/gofiber/fiber/v2"
 
-	"github.com/daltonscharff/spelling-bee-server/internal/handler"
+	"github.com/daltonscharff/spelling-bee-server/internal/handlers"
 )
 
 func DefineRoutes(app *fiber.App) error {
 	v1 := app.Group("/api/v1")
 
 	words := v1.Group("/words")
-	words.Get("/", handler.ViewAllWords)
-	words.Get("/:id", handler.ViewWord)
-	words.Post("/", handler.CreateWord)
-	words.Put("/:id", handler.UpdateWord)
-	words.Delete("/", handler.DeleteAllWords)
-	words.Delete("/:id", handler.DeleteWord)
+	words.Get("/", handlers.ViewAllWords)
+	words.Get("/:id", handlers.ViewWord)
+	words.Post("/", handlers.CreateWord)
+	words.Put("/:id", handlers.UpdateWord)
+	words.Delete("/", handlers.DeleteAllWords)
+	words.Delete("/:id", handlers.DeleteWord)
 
 	return nil
 }
