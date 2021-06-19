649070cfdef222cfd38ba96b34bb11691a773bc8
Dalton Scharff <dalton.scharff@gmail.com>
2021-06-18 21:52:13 -0500

    creates connection to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,34 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { WordsModule } from './api/words/words.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import databaseConfig from './config/database.config';
+import { Word } from './api/words/entities/word.entity';
 
 @Module({
   controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      load: [databaseConfig]
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres', 
+        host: config.get('database.host'),
+        port: +config.get<number>('database.port'),
+        username: config.get('database.username'),
+        password: config.get('database.password'),
+        database: config.get('database.database'),
+        entities: [
+          Word
+        ],
+        synchronize: true
+      }),
+      inject: [ConfigService],
+    }),
+    WordsModule
+  ],
 })
 export class AppModule {}
