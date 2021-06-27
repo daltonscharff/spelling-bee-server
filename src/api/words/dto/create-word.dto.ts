a189c2d916a99cbfa7727460e79c007bbfee7d9f
Dalton Scharff <dalton.scharff@gmail.com>
2021-06-27 08:14:47 -0500

    adds additional validation to CreateWordDto

--- src/api/words/dto/create-word.dto.ts
+++ src/api/words/dto/create-word.dto.ts
@@ -1,18 +1,23 @@
-import { IsString, IsInt } from 'class-validator';
+import { IsString, IsInt, IsPositive, IsOptional } from 'class-validator';
 
 export class CreateWordDto {
     @IsString()
     word: string;
 
     @IsInt()
+    @IsPositive()
+    @IsOptional()
     pointValue: number;
 
     @IsString()
+    @IsOptional()
     definition: string;
 
     @IsString()
+    @IsOptional()
     partOfSpeech: string;
 
     @IsString()
+    @IsOptional()
     synonym: string;
 }
